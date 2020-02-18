889200cfecfb5a9194db215d407db42360f54c8f
Lucas Lefrancq <lucaslefrancq@MacBook-Pro-de-Lucas.local>
2020-02-19 00:31:39 +0100

    test lib v2

--- test_minilibx/test.h
+++ test_minilibx/test.h
@@ -6,19 +6,22 @@
 /*   By: lucaslefrancq <lucaslefrancq@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 19:51:09 by lucaslefran       #+#    #+#             */
-/*   Updated: 2020/02/18 19:58:44 by lucaslefran      ###   ########.fr       */
+/*   Updated: 2020/02/19 00:22:43 by lucaslefran      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_H
 # define TEST_H
 
-typedef struct	s_rgb
+typedef struct	s_image
 {
-	int		rgb_int;
-	char	*red;
-	char	*green;
-	char	*blue;
-}				t_rgb;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	void	*img_ptr2;
+	int		x;
+	int		y;
+	int		color;
+}				t_image;
 
 #endif
